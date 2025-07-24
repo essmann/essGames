3cab8151b0d6a4075a897bf5d7a9fb68256d4790
Essmann <kenwilliam1000@gmail.com>
2025-07-24 19:50:31 +0200

    Made the game cards show title on hover

--- frontend/essGames/src/components/GameCard.jsx
+++ frontend/essGames/src/components/GameCard.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 function GameCard({ game }) {
   const { poster, title } = game;
+  const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <div className="game_card">
-      <img src={poster} alt={`${title} poster`} className="game_card_poster" />
+    <div className={`game_card ${isHovered ? 'selected' : ''}`}>
+      <img
+        src={poster}
+        alt={`${title} poster`}
+        className="game_card_poster"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      />
       <div className="game_card_title">{title}</div>
     </div>
   );
