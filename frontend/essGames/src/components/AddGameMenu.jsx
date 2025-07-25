05bc6f245dab1c1a762f694599aa2fc122c63600
Essmann <kenwilliam1000@gmail.com>
2025-07-25 17:06:15 +0200

    implemented add game function correctly

--- frontend/essGames/src/components/AddGameMenu.jsx
+++ frontend/essGames/src/components/AddGameMenu.jsx
@@ -5,6 +5,8 @@ import ClickAwayListener from "@mui/material/ClickAwayListener";
 import { useState } from "react";
 import CustomizedRating from "./CustomizedRating";
 import { useRef } from "react";
+import generateGuidInteger from "../database/generateGuidInteger";
+import handleAddGame from "../database/handleAddGame";
 const handleFileOpen = async (setFilePath) => {
   try {
     const dataUrl = await window.api.openImageFile() // Get Data URL from main process
@@ -18,24 +20,32 @@ const handleFileOpen = async (setFilePath) => {
     console.error("Failed to open file:", error);
   }
 };
-const addGame = async (game) => {
-  try {
-    const gameAdded = await window.api.addGame(game);
-    return gameAdded;
-  } catch (error){
-    console.error("Failed to add game: ", error);
-  }
-}
-// Usage example:
-handleGetGames().then((games) => {
-  console.log(games);
-});
 
-function AddGameMenu({ setAddGameMenuIsDisplayed }) {
+// Usage example:
+// handleGetGames().then((games) => {
+//   console.log(games);
+// });
+
+
+function AddGameMenu({ setAddGameMenuIsDisplayed, setGames }) {
   const [filePath, setFilePath] = useState("");
   const [rating, setRating] = useState(0);
   const [title, setTitle] = useState("");
   const titleRef = useRef(null);
+
+  const addGame = () => {
+                if(!validateInput){return;}
+                const title = titleRef.current.value;
+                const id = generateGuidInteger();
+                const game = {id: id, posterURL: filePath, rating: rating, review: "", title: title}
+                handleAddGame(game).then(()=>{
+                  setGames(prevGames => [...prevGames, game]);
+                })
+
+  }
+  const validateInput = () => {
+    return title !== null && id !== null && filePath !== null;
+  }
   return (
     <ClickAwayListener onClickAway={() => setAddGameMenuIsDisplayed(false)}>
       <div className="game_add_menu">
@@ -59,7 +69,14 @@ function AddGameMenu({ setAddGameMenuIsDisplayed }) {
                     <CustomizedRating onRating={(value)=>setRating(value)}/>
                 </div>
               </div>
+               <button className="game_add_menu_submit_btn" onClick={()=>{
+                addGame();
+            }}>
+               Submit </button>
+              
             </div>
+            
+            
           </>
         ) : (
           <>
@@ -80,15 +97,20 @@ function AddGameMenu({ setAddGameMenuIsDisplayed }) {
               
               <div className="game_add_menu_title_input add_info">
                 <div className="game_add_menu_title_label add_info">Game Title:</div>
-                <input type="text" />
+                <input type="text" ref={titleRef}/>
               </div>
               <div className="game_add_menu_rating">
                 <div className="game_add_menu_rating_label add_info">Your Rating:</div>
                 <div id="star_container_add">
-                    <CustomizedRating/>
+                   <CustomizedRating onRating={(value)=>setRating(value)}/>
                 </div>
               </div>
+            <button className="game_add_menu_submit_btn" onClick={()=>{
+              addGame();
+            }}>
+               Submit </button>
             </div>
+            
           </>
           
           
