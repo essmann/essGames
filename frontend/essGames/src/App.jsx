0e75206d992f0c7b263109ec8bc5c6b1274875f4
Essmann <kenwilliam1000@gmail.com>
2025-07-25 16:03:24 +0200

    Added sqlite3 connection

--- frontend/essGames/src/App.jsx
+++ frontend/essGames/src/App.jsx
@@ -5,15 +5,28 @@ import GameCard from './components/GameCard';
 import AddGameCard from './components/AddGameCard';
 import AddGameMenu from './components/AddGameMenu';
 import GameGrid from './components/GameGrid';
+import GameMenu from './components/GameMenu';
 function App() {
   const [selectedListItemIndex, setSelectedListItemIndex] = useState(0);
   const [addGameMenuIsDisplayed, setAddGameMenuIsDisplayed] = useState(false);
+  const [games, setGames] = useState([
+    { id: 1, rating: 3.5, title: "Dead Space", poster: "https://m.media-amazon.com/images/M/MV5BOGM4OTBhZjktYzAxZC00OTdlLWI2ZTEtZTliNjQ1NDg5OTJiXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg" },
+    { id: 2, rating: 4, title: "Minecraft", poster: "https://i.pinimg.com/474x/81/61/32/816132e3f5cf75f6ddd44b5f6536cdac.jpg" },
+    { id: 3, rating: 5, review: "this is my review", title: "World of Warcraft", poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/65/World_of_Warcraft.png/250px-World_of_Warcraft.png" }
+  ])
+const handleGetGames = async () => {
+  try {
+    const games = await window.api.getGames(); // Or window.electronAPI.invoke('get-games')
+    return games;
+  } catch (error) {
+    console.error("Failed to get games:", error);
+  }
+};
 
-  const games = [
-    { id: 1, title: "Dead Space", poster: "https://m.media-amazon.com/images/M/MV5BOGM4OTBhZjktYzAxZC00OTdlLWI2ZTEtZTliNjQ1NDg5OTJiXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg" },
-    { id: 2, title: "Minecraft", poster: "https://i.pinimg.com/474x/81/61/32/816132e3f5cf75f6ddd44b5f6536cdac.jpg" },
-    { id: 3, title: "World of Warcraft", poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/65/World_of_Warcraft.png/250px-World_of_Warcraft.png" }
-  ];
+// Usage example:
+handleGetGames().then((games) => {
+  console.log(games);
+});
 
   return (
     <>
@@ -26,7 +39,7 @@ function App() {
             case 0:
               return (
                <GameGrid games={games} setAddGameMenuIsDisplayed={setAddGameMenuIsDisplayed} />
-            // setAddGameMenuIsDisplayed={setAddGameMenuIsDisplayed}  
+               
             );
               case 1:
                 return (
@@ -41,8 +54,9 @@ function App() {
               return null;
           }
         })()}
-
+        
         {addGameMenuIsDisplayed && <AddGameMenu setAddGameMenuIsDisplayed={setAddGameMenuIsDisplayed} />}
+        <GameMenu/>
       </div>
     </>
   );
--- frontend/essGames/src/components/GameMenu.jsx
+++ frontend/essGames/src/components/GameMenu.jsx
@@ -1,31 +1,18 @@
-import Box from "@mui/material/Box";
-import Fab from "@mui/material/Fab";
-import AddIcon from "@mui/icons-material/Add";
+
 import ClickAwayListener from "@mui/material/ClickAwayListener";
+
 import { useState } from "react";
-import CustomizedRating from "./CustomizedRating";
-
-const handleFileOpen = async (setFilePath) => {
-  try {
-    const dataUrl = await window.electronAPI.openFile(); // Get Data URL from main process
-    if (dataUrl) {
-      setFilePath(dataUrl); // Set the Data URL as the src for the image
-      console.log(dataUrl); // Log the Data URL for debugging
-    } else {
-      setFilePath("No file selected");
-    }
-  } catch (error) {
-    console.error("Failed to open file:", error);
-  }
-};
-
 function GameMenu({ setAddGameMenuIsDisplayed }) {
-
+  const [gameMenuIsVisible, setGameMenuisVisible] = useState(true);
   return (
-    <ClickAwayListener >
+    <ClickAwayListener onClickAway={()=>setGameMenuisVisible(false)}>
+        {gameMenuIsVisible ?
         <div className="game_add_menu game_menu">
             test
         </div>
+        : <></>
+        
+      }
 
     </ClickAwayListener>
   );
