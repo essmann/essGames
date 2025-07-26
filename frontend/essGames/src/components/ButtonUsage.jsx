35ae2f7a4bc7a5c5d84b553a85953d56a33ec617
Essmann <kenwilliam1000@gmail.com>
2025-07-26 16:06:48 +0200

    Refactored child items and added a hierarchical parent child that can fit many children in the sidebar.

--- frontend/essGames/src/components/ButtonUsage.jsx
+++ frontend/essGames/src/components/ButtonUsage.jsx
@@ -1,28 +1,26 @@
 import { Icon } from "@mui/material";
-import VideogameAssetIcon from '@mui/icons-material/VideogameAsset';
-import ChatIcon from '@mui/icons-material/Chat';
-
+import VideogameAssetIcon from "@mui/icons-material/VideogameAsset";
+import ChatIcon from "@mui/icons-material/Chat";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import { useState } from "react";
+import { createContext } from "react";
+import { useGlobalContext } from "../Context/useGlobalContext";
 const selectItem = () => {
   // You might want to add some logic here
-}
+};
 
 function Sidebar({ selectedListItemIndex, setSelectedListItemIndex }) {
   return (
     <div className="sidebar">
       <div className="searchBar">
-        <ListItem
-          title="My Games"
-          icon={VideogameAssetIcon}
-          onClick={() => setSelectedListItemIndex(0)}
-          selected={selectedListItemIndex === 0}   // <-- here
+        <ListItem title="My Games" icon={VideogameAssetIcon} index={0} />
+        <ListItem title="My Reviews" icon={ChatIcon} index={1} />
+        <ListParentItem title="Malene" icon={ChatIcon} index={2}>
+          <ListItem isChild={true} title="Child2" icon={ChatIcon} index={3} />
+          <ListItem isChild={true} title="Child3" icon={ChatIcon} index={4} />
+          <ListItem isChild={true} title="Child4" icon={ChatIcon} index={5} />
 
-        />
-        <ListItem
-          title="My Reviews"
-          icon={ChatIcon}
-          onClick={() => setSelectedListItemIndex(1)}
-          selected={selectedListItemIndex === 1}   // <-- here
-        />
+        </ListParentItem>
       </div>
     </div>
   );
@@ -30,12 +28,22 @@ function Sidebar({ selectedListItemIndex, setSelectedListItemIndex }) {
 
 export default Sidebar;
 
-function ListItem({ title, icon, onClick, selected }) {
+function ListItem({ title, icon, isChild, index }) {
+  const { selectedListItemIndex, setSelectedListItemIndex } =
+    useGlobalContext();
+  const whenClicked = () => {
+    if (selectedListItemIndex == index) {
+      return;
+    }
+    setSelectedListItemIndex(index);
+  };
+  var selected = selectedListItemIndex == index;
   return (
     <div
-      className="list_item"
-      onClick={onClick}
-      style={{ background: selected ? '#2D2D2D' : '#202020', cursor: 'pointer' }} // blue if selected, else black
+      className={`list_item ${selected ? "selected" : ""} ${
+        isChild ? "list_item_child" : ""
+      }`}
+      onClick={whenClicked}
     >
       <div className="list_item_icon">
         <Icon component={icon} />
@@ -45,3 +53,48 @@ function ListItem({ title, icon, onClick, selected }) {
   );
 }
 
+function ListParentItem({
+  title,
+  icon,
+  onExpand,
+  onCollapse,
+  index,
+  children,
+}) {
+  const { selectedListItemIndex, setSelectedListItemIndex } =
+    useGlobalContext();
+    const whenClicked = () => {
+    if (selectedListItemIndex == index) {
+      return;
+    }
+    setSelectedListItemIndex(index);
+  };
+  const [isExpanded, setIsExpanded] = useState(false);
+  var selected = selectedListItemIndex == index;
+
+  return (
+    <div
+      className="list_item_parent_container"
+      
+    >
+      <div
+        className={`list_item list_item_parent ${selected ? "selected" : ""}`}
+        onClick={() => {
+          setIsExpanded((prev) => !prev);
+          whenClicked();
+        }}
+        style={{
+          background: selected ? "#2D2D2D" : "#202020",
+          cursor: "pointer",
+        }} // blue if selected, else black
+      >
+        <div className="list_item_icon">
+          <Icon component={icon} />
+        </div>
+        <div className="list_item_title">{title}</div>
+        <ArrowDropDownIcon />
+      </div>
+      {isExpanded && children}
+    </div>
+  );
+}
